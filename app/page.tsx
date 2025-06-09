54f1a5aaac6f7192bdf4e7c4458dd55c5c2817fb
Durgesh Tiwari <twdurgesh226@gmail.com>
2025-06-09 12:47:59 +0530

    Durgesh commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
 
 // app/page.tsx
 import AboutUs from './components/aboutus';
-import ComparisonSlider from './components/ComparisonSlider';
-import CircularWrapper from './components/circularWrapper';
-import ImageGrid from './components/grid';
 
-// Initialize the font
-const CollageIframe = () => {
-  return (
-    <iframe
-      src="http://localhost:3001"
-      width="100%"
-      height="600px"
-      style={{ border: "none" }}
-    />
-  );
-};
+
+
 
 
 
